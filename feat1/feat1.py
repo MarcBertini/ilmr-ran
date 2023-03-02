9924b1a354b1f23dc2e3db362fe87285c056cfb3
Marc Bertini <bertini_marc@yahoo.fr>
2023-03-02 13:03:58 +0100

    feat:(master) feat1 skeleton

--- feat1/feat1.py
+++ feat1/feat1.py
@@ -1,1 +1,10 @@
 # Initialize feature 1 implementation in py
+
+# import the necessary packages
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
